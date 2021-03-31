33594d48d1dad873fad9e70fce6d2c4b51f71f59
PedroKBrant <pedrokbrant@gmail.com>
2021-03-30 23:54:04 -0300

    first commit

--- pkb_tracking/pkb_tracking.cpp
+++ pkb_tracking/pkb_tracking.cpp
@@ -1,92 +1,283 @@
+#include <opencv2/features2d.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/highgui.hpp>      //for imshow
+#include <vector>
 #include <iostream>
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/features2d.hpp"
-#include "opencv2/calib3d.hpp"
-#include "opencv2/imgproc.hpp"
-using namespace cv;
+#include <iomanip>
+
+#include "stats.h" // Stats structure definition
+#include "utils.h" // Drawing and printing functions
+
 using std::cout;
-using std::endl;
+using std::vector;
+using std::string;
+
+using namespace std;
+using namespace cv;
+
+
+const double kDistanceCoef = 4.0;
+const int kMaxMatchingSize = 50;
+
+
+const double akaze_thresh = 3e-4; // AKAZE detection threshold set to locate about 1000 keypoints
+const double ransac_thresh = 2.5f; // RANSAC inlier threshold
+const double nn_match_ratio = 0.8f; // Nearest-neighbour matching ratio
+const int bb_min_inliers = 100; // Minimal number of inliers to draw bounding box
+const int stats_update_period = 10; // On-screen statistics are updated every 10 frames
+
+
+Ptr<Feature2D> featureDetector(string type) {
+    if (type.find("fast") == 0) {
+        type = type.substr(4);
+        Ptr<FastFeatureDetector> detector = FastFeatureDetector::create(10, true);
+        return detector;
+    }
+    if (type.find("blob") == 0) {
+        type = type.substr(4);
+        Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create();
+        return detector;
+    }
+    if (type == "orb") {
+        Ptr<ORB> orb = ORB::create();
+        return orb;
+    }
+    if (type == "brisk") {
+        Ptr<BRISK> brisk = BRISK::create();
+        return brisk;
+    }
+    if (type == "kaze") {
+        Ptr<KAZE> kaze = KAZE::create();
+        return kaze;
+    }
+    if (type == "akaze") {
+        Ptr<AKAZE> akaze = AKAZE::create();
+        akaze->setThreshold(akaze_thresh);
+        return akaze;
+    }
+}
+
+void match(string type, Mat& desc1, Mat& desc2, vector<DMatch>& matches) {
+    matches.clear();
+    if (type == "bf") {
+        BFMatcher desc_matcher(cv::NORM_HAMMING, true);
+        desc_matcher.match(desc1, desc2, matches, Mat());
+    }
+    if (type == "knn") {
+        BFMatcher desc_matcher(cv::NORM_HAMMING, true);
+        vector< vector<DMatch> > vmatches;
+        desc_matcher.knnMatch(desc1, desc2, vmatches, 1);
+        for (int i = 0; i < static_cast<int>(vmatches.size()); ++i) {
+            if (!vmatches[i].size()) {
+                continue;
+            }
+            matches.push_back(vmatches[i][0]);
+        }
+    }
+    std::sort(matches.begin(), matches.end());
+    while (matches.front().distance * kDistanceCoef < matches.back().distance) {
+        matches.pop_back();
+    }
+    while (matches.size() > kMaxMatchingSize) {
+        matches.pop_back();
+    }
+}
+
+void findKeyPointsHomography(vector<KeyPoint>& kpts1, vector<KeyPoint>& kpts2,
+    vector<DMatch>& matches, vector<char>& match_mask) {
+    if (static_cast<int>(match_mask.size()) < 3) {
+        return;
+    }
+    vector<cv::Point2f> pts1;
+    vector<cv::Point2f> pts2;
+    for (int i = 0; i < static_cast<int>(matches.size()); ++i) {
+        pts1.push_back(kpts1[matches[i].queryIdx].pt);
+        pts2.push_back(kpts2[matches[i].trainIdx].pt);
+    }
+    findHomography(pts1, pts2, cv::RANSAC, 4, match_mask);
+}
+
+
+namespace example {
+    class Tracker
+    {
+    public:
+        Tracker(Ptr<Feature2D> _detector, Ptr<DescriptorMatcher> _matcher) :
+            detector(_detector),
+            matcher(_matcher)
+        {}
+
+        void setFirstFrame(const Mat frame, vector<Point2f> bb, string title, Stats& stats);
+        Mat process(const Mat frame, Stats& stats);
+        Ptr<Feature2D> getDetector() {
+            return detector;
+        }
+    protected:
+        Ptr<Feature2D> detector;
+        Ptr<DescriptorMatcher> matcher;
+        Mat first_frame, first_desc;
+        vector<KeyPoint> first_kp;
+        vector<Point2f> object_bb;
+    };
+
+    void Tracker::setFirstFrame(const Mat frame, vector<Point2f> bb, string title, Stats& stats)
+    {
+        cv::Point* ptMask = new cv::Point[bb.size()];
+        const Point* ptContain = { &ptMask[0] };
+        int iSize = static_cast<int>(bb.size());
+        for (size_t i = 0; i < bb.size(); i++) {
+            ptMask[i].x = static_cast<int>(bb[i].x);
+            ptMask[i].y = static_cast<int>(bb[i].y);
+        }
+        first_frame = frame.clone();
+        cv::Mat matMask = cv::Mat::zeros(frame.size(), CV_8UC1);
+        cv::fillPoly(matMask, &ptContain, &iSize, 1, cv::Scalar::all(255));
+        detector->detectAndCompute(first_frame, matMask, first_kp, first_desc);
+        stats.keypoints = (int)first_kp.size();
+        drawBoundingBox(first_frame, bb);
+        putText(first_frame, title, Point(0, 60), FONT_HERSHEY_PLAIN, 5, Scalar::all(0), 4);
+        object_bb = bb;
+        delete[] ptMask;
+    }
+
+    Mat Tracker::process(const Mat frame, Stats& stats)
+    {
+        TickMeter tm;
+        vector<KeyPoint> kp;
+        Mat desc;
+
+        tm.start();
+        detector->detectAndCompute(frame, noArray(), kp, desc);
+        stats.keypoints = (int)kp.size();
+
+        vector< vector<DMatch> > matches;
+        vector<KeyPoint> matched1, matched2;
+        matcher->knnMatch(first_desc, desc, matches, 2);
+        for (unsigned i = 0; i < matches.size(); i++) {
+            if (matches[i][0].distance < nn_match_ratio * matches[i][1].distance) {
+                matched1.push_back(first_kp[matches[i][0].queryIdx]);
+                matched2.push_back(kp[matches[i][0].trainIdx]);
+            }
+        }
+        stats.matches = (int)matched1.size();
+
+        Mat inlier_mask, homography;
+        vector<KeyPoint> inliers1, inliers2;
+        vector<DMatch> inlier_matches;
+        if (matched1.size() >= 4) {
+            homography = findHomography(Points(matched1), Points(matched2),
+                RANSAC, ransac_thresh, inlier_mask);
+        }
+        tm.stop();
+        stats.fps = 1. / tm.getTimeSec();
+
+        if (matched1.size() < 4 || homography.empty()) {
+            Mat res;
+            hconcat(first_frame, frame, res);
+            stats.inliers = 0;
+            stats.ratio = 0;
+            return res;
+        }
+        for (unsigned i = 0; i < matched1.size(); i++) {
+            if (inlier_mask.at<uchar>(i)) {
+                int new_i = static_cast<int>(inliers1.size());
+                inliers1.push_back(matched1[i]);
+                inliers2.push_back(matched2[i]);
+                inlier_matches.push_back(DMatch(new_i, new_i, 0));
+            }
+        }
+        stats.inliers = (int)inliers1.size();
+        stats.ratio = stats.inliers * 1.0 / stats.matches;
+
+        vector<Point2f> new_bb;
+        perspectiveTransform(object_bb, new_bb, homography);
+        Mat frame_with_bb = frame.clone();
+        if (stats.inliers >= bb_min_inliers) {
+            drawBoundingBox(frame_with_bb, new_bb);
+        }
+        Mat res;
+        drawMatches(first_frame, inliers1, frame_with_bb, inliers2,
+            inlier_matches, res,
+            Scalar(255, 0, 0), Scalar(255, 0, 0));
+        return res;
+    }
+}
 
 const char* keys =
 "{ help h |                  | Print help message. }"
-"{ input1 | box.png          | Path to input image 1. }"
-"{ input2 | box_in_scene.png | Path to input image 2. }";
+"{ input1 | kaze             | Path to input image 1. }"
+"{ input2 | knn              | Path to input image 2. }";
 
-int main(int argc, char* argv[]){
-    CommandLineParser parser(argc, argv, keys);
-    Mat img_object = imread(samples::findFile(parser.get<String>("input1")), IMREAD_GRAYSCALE);
-    Mat img_scene = imread(samples::findFile(parser.get<String>("input2")), IMREAD_GRAYSCALE);
-    if (img_object.empty() || img_scene.empty()){
-        cout << "Could not open or find the image!\n" << endl;
-        parser.printMessage();
-        return -1;
+int main(int argc, char** argv){
+
+    cout << "Type: FeatureDetector, Match Algorithm respectively";
+    CommandLineParser parser(argc, argv, "{@input_path |0|input path can be a camera id, like 0,1,2 or a video filename}");
+    parser.printMessage();
+    string input_path = parser.get<string>(0);
+    string video_name = input_path;
+
+    VideoCapture video_in;
+
+    if ((isdigit(input_path[0]) && input_path.size() == 1))
+    {
+        int camera_no = input_path[0] - '0';
+        video_in.open(camera_no);
     }
-    //-- Step 1: Detect the keypoints using AKAZE and ORB Detector
-    Ptr<AKAZE> detector = AKAZE::create();
-    //TODO add a selector to determine the desired detector
-    //Ptr<ORB> detector = ORB::create();
-    //Ptr<BRISK> detector = BRISK::create();
-    std::vector<KeyPoint> keypoints_object, keypoints_scene;
-    Mat descriptors1, descriptors2;
-    detector->detectAndCompute(img_object, noArray(), keypoints_object, descriptors1);
-    detector->detectAndCompute(img_scene, noArray(), keypoints_scene, descriptors2);
-    //-- Step 2: Matching descriptor vectors with a brute force matcher
-    //TODO test diferent matchers 
-    BFMatcher matcher(NORM_HAMMING);
-    std::vector< std::vector<DMatch> > knn_matches;
-    matcher.knnMatch(descriptors1, descriptors2, knn_matches, 2);
-    //-- Step 3: Filter matches using the Lowe's ratio test
-    // TODO 1 cross check test and 2 geometric test
-    const float ratio_thresh = 0.7f;
-    std::vector<DMatch> good_matches;
-    for (size_t i = 0; i < knn_matches.size(); i++){
-        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance){
-            good_matches.push_back(knn_matches[i][0]);
+    else {
+        video_in.open(video_name);
+    }
+
+    if (!video_in.isOpened()) {
+        cerr << "Couldn't open " << video_name << endl;
+        return 1;
+    }
+
+    Stats stats, detector_stats;
+    Ptr<Feature2D> detector = featureDetector("orb");
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+    example::Tracker tracker(detector, matcher);
+
+    Mat frame;
+    namedWindow(video_name, WINDOW_NORMAL);
+    cout << "\nPress any key to stop the video and select a bounding box" << endl;
+
+    while (waitKey(1) < 1)
+    {
+        video_in >> frame;
+        cv::resizeWindow(video_name, frame.size());
+        imshow(video_name, frame);
+    }
+    vector<Point2f> bb;
+    cv::Rect uBox = cv::selectROI(video_name, frame);
+    bb.push_back(cv::Point2f(static_cast<float>(uBox.x), static_cast<float>(uBox.y)));
+    bb.push_back(cv::Point2f(static_cast<float>(uBox.x + uBox.width), static_cast<float>(uBox.y)));
+    bb.push_back(cv::Point2f(static_cast<float>(uBox.x + uBox.width), static_cast<float>(uBox.y + uBox.height)));
+    bb.push_back(cv::Point2f(static_cast<float>(uBox.x), static_cast<float>(uBox.y + uBox.height)));
+
+    tracker.setFirstFrame(frame, bb, "ORB", stats);
+    Stats draw_stats;
+    Mat res;
+    int i = 0;
+    for (;;) {
+        i++;
+        bool update_stats = (i % stats_update_period == 0);
+        video_in >> frame;
+        // stop the program if no more images
+        if (frame.empty()) break;
+
+        res = tracker.process(frame, stats);
+        detector_stats += stats;
+        if (update_stats) {
+            draw_stats = stats;
         }
+        drawStatistics(res, draw_stats);
+        cv::imshow(video_name, res);
+
+        if (waitKey(1) == 27) break; //quit on ESC button
     }
-    //-- Draw matches
-    //without filter
-    //Mat img_matches;
-    //drawMatches(img1, keypoints_object, img2, keypoints_scene, knn_matches, img_matches);
-    //with lowe's filter
-    Mat img_good_matches;
-    drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_good_matches, Scalar::all(-1),
-        Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-
-    //-- Localize the object
-    std::vector<Point2f> obj;
-    std::vector<Point2f> scene;
-    for (size_t i = 0; i < good_matches.size(); i++){
-        //-- Get the keypoints from the good matches
-        obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
-        scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
-    }
-
-    Mat H = findHomography(obj, scene, RANSAC);
-    //-- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<Point2f> obj_corners(4);
-    obj_corners[0] = Point2f(0, 0);
-    obj_corners[1] = Point2f((float)img_object.cols, 0);
-    obj_corners[2] = Point2f((float)img_object.cols, (float)img_object.rows);
-    obj_corners[3] = Point2f(0, (float)img_object.rows);
-    std::vector<Point2f> scene_corners(4);
-    perspectiveTransform(obj_corners, scene_corners, H);
-    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    line(img_good_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),
-        scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_good_matches, scene_corners[1] + Point2f((float)img_object.cols, 0),
-        scene_corners[2] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_good_matches, scene_corners[2] + Point2f((float)img_object.cols, 0),
-        scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_good_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),
-        scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4);
-
-
-    //-- Show detected matches
-   //imshow("No_filter", img_matches);
-    imshow("Filter", img_good_matches);
-    waitKey();
+    stats /= i - 1;
+    printStatistics("ORB", stats);
     return 0;
 }
